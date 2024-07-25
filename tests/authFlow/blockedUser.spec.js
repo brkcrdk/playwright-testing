16cd193cb8476e2c8bca200736c6bff3ba0594ac
tugay.demirel <tugay@90pixel.com>
2024-07-25 18:06:41 +0300

    UPDATE: Improving new case

--- tests/authFlow/blockedUser.spec.js
+++ tests/authFlow/blockedUser.spec.js
@@ -1,44 +1,11 @@
 import { test, expect } from "@playwright/test";
+import signIn from "../../signIn";
 
 test.beforeEach(async ({ page }) => {
   await page.goto(process.env.BASE_URL);
 });
 
 test.describe("Test blocked user authentication flow", () => {
-  test("When blocked user tries to login, should see blocked warning", async ({
-    page,
-  }) => {
-    await page.goto(`${process.env.BASE_URL}/users/sign_in`);
-    await page
-      .locator('[data-qa-selector="login_field"]')
-      .fill(process.env.BLOCKED_USER_USER_NAME);
-    await page
-      .locator('[data-qa-selector="password_field"]')
-      .fill(process.env.BLOCKED_USER_PASSWORD);
-    await page.locator('[data-qa-selector="sign_in_button"]').click();
-    await expect(
-      page.locator('[data-qa-selector="flash_container"]')
-    ).toHaveText(
-      "Your account has been blocked. Please contact your GitLab administrator if you think this is an error."
-    );
-  });
-
-  // test("Should redirect me to warning page when blocked after logged in", async ({
-  //   page,
-  // }) => {
-  //   // await expect(page).toHaveURL(/users\/sign_in/);
-  //   // await expect(
-  //   //   page.locator('[data-qa-selector="flash_container"]')
-  //   // ).toHaveText(
-  //   //   "Your account has been blocked. Please contact your GitLab administrator if you think this is an error."
-  //   // );
-  // });
-
-  // Blocklanıcak kullanıcı başta normal giriş yapar
-  // Admin bu kullanıcıyı bloklar
-  // Kullanıcı sayfada herhangi bir işlem yaptığında login sayfasına yönlenir ve blocklandığına dair bir warning görür
-  // Admin kullanıcıyı unblocklar
-  // Kullanıcı tekrar normal şekilde giriş yapabilir
   test("Should redirect me to warning page when blocked after logged in", async ({
     browser,
   }) => {
@@ -59,6 +26,7 @@ test.describe("Test blocked user authentication flow", () => {
     });
     const adminPage = await adminContext.newPage();
 
+    // Admin kullanıcıyı bloklar
     await adminPage.goto(process.env.BASE_URL);
     await expect(adminPage.getByTestId("admin-icon")).toBeVisible();
     await adminPage.getByTestId("admin-icon").click();
@@ -79,5 +47,50 @@ test.describe("Test blocked user authentication flow", () => {
       .click();
     await expect(adminPage).toHaveURL(/users\/aysel/);
     await adminPage.getByRole("button", { name: "Block user" }).click();
+    const confirmBlock = adminPage.getByRole("button", {
+      name: "Block",
+      exact: true,
+    });
+    await expect(confirmBlock).toBeVisible();
+    await confirmBlock.click();
+    await expect(
+      adminPage.locator('[data-qa-selector="flash_container"]')
+    ).toHaveText("Successfully blocked");
+
+    // Bloklu kullanıcı sayfa yeniler ve logine yönlenir
+    await blockedUserPage.reload();
+    await expect(blockedUserPage).toHaveURL(/users\/sign_in/);
+    await expect(
+      blockedUserPage.locator('[data-qa-selector="flash_container"]')
+    ).toHaveText(
+      "Your account has been blocked. Please contact your GitLab administrator if you think this is an error."
+    );
+
+    // Kullanıcının bloğunu kaldırıyoruz
+    const unblockButton = adminPage.getByRole("button", {
+      name: "Unblock user",
+    });
+    await expect(unblockButton).toHaveClass(
+      "btn gl-button btn-info js-confirm-modal-button"
+    );
+    await unblockButton.click();
+    const confirmUnblock = adminPage.getByRole("button", {
+      name: "Unblock",
+      exact: true,
+    });
+    await expect(confirmUnblock).toBeVisible();
+    await confirmUnblock.click();
+    await expect(
+      adminPage.locator('[data-qa-selector="flash_container"]')
+    ).toHaveText("Successfully unblocked");
+
+    // Bloklu kullanıcı tekrar normal şekilde giriş yapabilir
+    await blockedUserPage.goto(`${process.env.BASE_URL}/users/sign_in`);
+    await signIn({
+      page: blockedUserPage,
+      username: process.env.BLOCKED_USER_USER_NAME,
+      password: process.env.BLOCKED_USER_PASSWORD,
+      path: "playwright/.auth/blockedUser.json",
+    });
   });
 });
